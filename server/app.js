b89f6d395ff3785ad66e4de911651958396de4a0
StepahnieMangin <samangin89@gmail.com>
2016-03-30 13:45:19 -0400

    Retrieve Notes from MongoDB

--- server/app.js
+++ server/app.js
@@ -1,32 +1,37 @@
 var express = require('express');
 var notelyServerApp = express();
 
-// CORS - cross origin resource sharing middleware
+var db = require('mongoose');
+db.connect('mongodb://localhost:27017/notely');
+
+var NoteSchema = db.Schema({
+  title: String,
+  body_html: String
+});
+
+var Note = db.model('Note', NoteSchema);
+
+// Cross-Origin Resource Sharing (CORS) middleware
 notelyServerApp.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
 notelyServerApp.get('/', function(req, res) {
-  //res.send('I <3 Express!');
-  res.json([
-      {
-        "title": "Note 1 (hard-code)",
-        "body_html" : "Body 1 (hard-code)"
-      },
-      {
-        "title": "Note 2 (hard-code)",
-        "body_html" : "Body 2 (hard-code)"
-      },
-      {
-        "title": "Note 3 (hard-code)",
-        "body_html" : "Body 3 (hard-code)"
-      },
-      {
-        "title": "Note 4 (hard-code)",
-        "body_html" : "Body 4 (hard-code)"
-      }
-    ]);
+  Note.find().then(function(notes) {
+    res.json(notes);
+  });
+
+  // res.json([
+  //   {
+  //     title: 'Edited hardcoded note!',
+  //     body_html: 'Cool note. Aww, shucks.'
+  //   },
+  //   {
+  //     title: 'Another edited hardcoded note',
+  //     body_html: "Ain't life grand?"
+  //   }
+  // ]);
 });
 
 notelyServerApp.listen(3030, function() {
